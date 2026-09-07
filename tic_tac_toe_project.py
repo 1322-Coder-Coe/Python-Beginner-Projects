4ed23bc8d6a9eaacf6db5e1b26b827d411fff0e5
1322-Coder-Coe <amazon.ducalian007@gmail.com>
2026-09-07 11:42:06 -0600

    created user input logic and mapped grid

--- tic_tac_toe_project.py
+++ tic_tac_toe_project.py
@@ -10,4 +10,29 @@ board_grid =[
 for row in board_grid:
     print(row)
 
+#creating user input logic
+choice = int(input("which space would you like to mark? "))
 
+# Mapping choice to grid locations
+if choice == 1:
+    board_grid[0][0] = "X"
+elif choice == 2:
+    board_grid[0][1] = "X"
+elif choice == 3:
+    board_grid[0][2] = "X"
+elif choice == 4:
+    board_grid[1][0] = "X"
+elif choice == 5:
+    board_grid[1][1] = "X"
+elif choice == 6:
+    board_grid[1][2] = "X"
+elif choice == 7:
+    board_grid[2][0] = "X"
+elif choice == 8:
+    board_grid[2][1] = "X"
+elif choice == 9:
+    board_grid[2][2] = "X"
+    
+
+                
+               
