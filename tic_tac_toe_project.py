9ca590bf8698467f62cb318ceccf0a45ce20c078
1322-Coder-Coe <amazon.ducalian007@gmail.com>
2026-09-07 10:26:25 -0600

    Created board grid and display

---
+++ tic_tac_toe_project.py
@@ -0,0 +1,13 @@
+# creating board spaces (3 rows of 3)
+board_grid =[ 
+    [1,2,3],
+    [4,5,6],
+    [7,8,9] 
+    ]
+ 
+
+#displaying board as 3x3 grid
+for row in board_grid:
+    print(row)
+
+
