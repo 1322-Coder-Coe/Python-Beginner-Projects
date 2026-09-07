3819533f5b3a659d8dde18d6cdbca94aca3494d2
1322-Coder-Coe <amazon.ducalian007@gmail.com>
2026-09-07 17:36:46 -0600

    solved all win conditions and draw condition

--- tic_tac_toe_project.py
+++ tic_tac_toe_project.py
@@ -15,6 +15,8 @@ update()
 #creating space under grid
 print()
 
+#creating counter
+turn_count = 0
 #creating player input loop
 
 while True:
@@ -29,6 +31,7 @@ while True:
             continue
         else: 
             board_grid[0][0] = "X"
+            turn_count += 1
             
     elif player_1 == 2:
         if board_grid[0][1] == "X" or board_grid[0][1] == "O": 
@@ -37,6 +40,7 @@ while True:
             continue
         else: 
             board_grid[0][1] = "X"
+            turn_count += 1
             
     elif player_1 == 3:
         if board_grid[0][2] == "X" or board_grid[0][2] == "O": 
@@ -45,6 +49,7 @@ while True:
             continue
         else: 
             board_grid[0][2] = "X"
+            turn_count += 1
             
     elif player_1 == 4:
         if board_grid[1][0] == "X" or board_grid[1][0] == "O": 
@@ -53,6 +58,7 @@ while True:
             continue
         else: 
             board_grid[1][0] = "X"
+            turn_count += 1
             
     elif player_1 == 5:
         if board_grid[1][1] == "X" or board_grid[1][1] == "O": 
@@ -61,6 +67,7 @@ while True:
             continue
         else: 
             board_grid[1][1] = "X"
+            turn_count += 1
             
     elif player_1 == 6:
         if board_grid[1][2] == "X" or board_grid[1][2] == "O": 
@@ -69,6 +76,7 @@ while True:
             continue
         else: 
             board_grid[1][2] = "X"
+            turn_count += 1
             
     elif player_1 == 7:
         if board_grid[2][0] == "X" or board_grid[2][0] == "O": 
@@ -77,6 +85,7 @@ while True:
             continue
         else: 
              board_grid[2][0] = "X"
+             turn_count += 1
              
     elif player_1 == 8:
         if board_grid[2][1] == "X" or board_grid[2][1] == "O": 
@@ -85,6 +94,7 @@ while True:
             continue
         else: 
              board_grid[2][1] = "X"
+             turn_count += 1
              
     elif player_1 == 9:
         if board_grid[2][2] == "X" or board_grid[2][2] == "O": 
@@ -93,20 +103,45 @@ while True:
             continue
         else: 
              board_grid[2][2] = "X"
-
+             turn_count += 1
 #creating space under display
     print()
 
 # displaying user input mark in grid 
     update()
     #applying test for win in first loop
-    if board_grid[0][0] == "X" and board_grid[0][1] == "X" and board_grid[0][2] == "X":
+        # Check all Player 1 win conditions
+    if (
+        (board_grid[0][0] == "X" and board_grid[0][1] == "X" and board_grid[0][2] == "X") or # Top row
+        (board_grid[1][0] == "X" and board_grid[1][1] == "X" and board_grid[1][2] == "X") or # Middle row
+        (board_grid[2][0] == "X" and board_grid[2][1] == "X" and board_grid[2][2] == "X") or # Bottom row
+        (board_grid[0][0] == "X" and board_grid[1][0] == "X" and board_grid[2][0] == "X") or # Left column
+        (board_grid[0][1] == "X" and board_grid[1][1] == "X" and board_grid[2][1] == "X") or # Middle column
+        (board_grid[0][2] == "X" and board_grid[1][2] == "X" and board_grid[2][2] == "X") or # Right column
+        (board_grid[0][0] == "X" and board_grid[1][1] == "X" and board_grid[2][2] == "X") or # Diagonal top-left to bottom-right
+        (board_grid[0][2] == "X" and board_grid[1][1] == "X" and board_grid[2][0] == "X")    # Diagonal top-right to bottom-left
+    ):
         print("Congratulations Player 1! You Won")
         break
 
-    elif board_grid[0][0] == "O" and board_grid[0][1] == "O" and board_grid[0][2] == "O":
+    # Check all Player 2 win conditions
+    elif (
+        (board_grid[0][0] == "O" and board_grid[0][1] == "O" and board_grid[0][2] == "O") or 
+        (board_grid[1][0] == "O" and board_grid[1][1] == "O" and board_grid[1][2] == "O") or 
+        (board_grid[2][0] == "O" and board_grid[2][1] == "O" and board_grid[2][2] == "O") or 
+        (board_grid[0][0] == "O" and board_grid[1][0] == "O" and board_grid[2][0] == "O") or 
+        (board_grid[0][1] == "O" and board_grid[1][1] == "O" and board_grid[2][1] == "O") or 
+        (board_grid[0][2] == "O" and board_grid[1][2] == "O" and board_grid[2][2] == "O") or 
+        (board_grid[0][0] == "O" and board_grid[1][1] == "O" and board_grid[2][2] == "O") or 
+        (board_grid[0][2] == "O" and board_grid[1][1] == "O" and board_grid[2][0] == "O")
+    ):
         print("Congratulations Player 2! You Won")
-        break          
+        break
+        
+    elif turn_count == 9:
+        print("It's a Draw!")
+        break
+
 #creating space
     print()          
     
@@ -121,7 +156,8 @@ while True:
                 print()
                 continue
             else: 
-                board_grid[0][0] = "O"
+                board_grid[0][0] = "O"            
+                turn_count += 1
                 break
                 
         elif player_2 == 2:
@@ -131,6 +167,7 @@ while True:
                 continue
             else: 
                 board_grid[0][1] = "O"
+                turn_count += 1
                 break
                 
         elif player_2 == 3:
@@ -140,6 +177,7 @@ while True:
                 continue
             else: 
                 board_grid[0][2] = "O"
+                turn_count += 1
                 break
                 
         elif player_2 == 4:
@@ -149,6 +187,7 @@ while True:
                 continue
             else: 
                 board_grid[1][0] = "O"
+                turn_count += 1
                 break
                 
         elif player_2 == 5:
@@ -158,6 +197,7 @@ while True:
                 continue
             else: 
                 board_grid[1][1] = "O"
+                turn_count += 1
                 break
                 
         elif player_2 == 6:
@@ -167,6 +207,7 @@ while True:
                 continue
             else: 
                 board_grid[1][2] = "O"
+                turn_count += 1
                 break
                 
         elif player_2 == 7:
@@ -176,6 +217,7 @@ while True:
                 continue
             else: 
                 board_grid[2][0] = "O"
+                turn_count += 1
                 break
                 
         elif player_2 == 8:
@@ -185,6 +227,7 @@ while True:
                 continue
             else: 
                 board_grid[2][1] = "O"
+                turn_count += 1
                 break
                 
         elif player_2 == 9:
@@ -194,6 +237,7 @@ while True:
                 continue
             else: 
                 board_grid[2][2] = "O"
+                turn_count += 1
                 break
 
     # creating space under display
@@ -204,10 +248,34 @@ while True:
     print()
 
       
-    if board_grid[0][0] == "X" and board_grid[0][1] == "X" and board_grid[0][2] == "X":
+ # Check all Player 1 win conditions
+    if (
+        (board_grid[0][0] == "X" and board_grid[0][1] == "X" and board_grid[0][2] == "X") or # Top row
+        (board_grid[1][0] == "X" and board_grid[1][1] == "X" and board_grid[1][2] == "X") or # Middle row
+        (board_grid[2][0] == "X" and board_grid[2][1] == "X" and board_grid[2][2] == "X") or # Bottom row
+        (board_grid[0][0] == "X" and board_grid[1][0] == "X" and board_grid[2][0] == "X") or # Left column
+        (board_grid[0][1] == "X" and board_grid[1][1] == "X" and board_grid[2][1] == "X") or # Middle column
+        (board_grid[0][2] == "X" and board_grid[1][2] == "X" and board_grid[2][2] == "X") or # Right column
+        (board_grid[0][0] == "X" and board_grid[1][1] == "X" and board_grid[2][2] == "X") or # Diagonal top-left to bottom-right
+        (board_grid[0][2] == "X" and board_grid[1][1] == "X" and board_grid[2][0] == "X")    # Diagonal top-right to bottom-left
+    ):
         print("Congratulations Player 1! You Won")
         break
 
-    elif board_grid[0][0] == "O" and board_grid[0][1] == "O" and board_grid[0][2] == "O":
+    # Check all Player 2 win conditions
+    elif (
+        (board_grid[0][0] == "O" and board_grid[0][1] == "O" and board_grid[0][2] == "O") or 
+        (board_grid[1][0] == "O" and board_grid[1][1] == "O" and board_grid[1][2] == "O") or 
+        (board_grid[2][0] == "O" and board_grid[2][1] == "O" and board_grid[2][2] == "O") or 
+        (board_grid[0][0] == "O" and board_grid[1][0] == "O" and board_grid[2][0] == "O") or 
+        (board_grid[0][1] == "O" and board_grid[1][1] == "O" and board_grid[2][1] == "O") or 
+        (board_grid[0][2] == "O" and board_grid[1][2] == "O" and board_grid[2][2] == "O") or 
+        (board_grid[0][0] == "O" and board_grid[1][1] == "O" and board_grid[2][2] == "O") or 
+        (board_grid[0][2] == "O" and board_grid[1][1] == "O" and board_grid[2][0] == "O")
+    ):
         print("Congratulations Player 2! You Won")
+        break
+        
+    elif turn_count == 9:
+        print("It's a Draw!")
         break
